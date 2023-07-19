8646cf6ca07ca22166e94c39981749edab70be37
IlyaPoly <132166094+IlyaPoly@users.noreply.github.com>
2023-07-19 12:58:02 +0300

    Update trie.h

--- ConsoleApplication4/trie.h
+++ ConsoleApplication4/trie.h
@@ -1,1 +1,26 @@
+#pragma once
+#ifndef TRIE_H
+#define TRIE_H
 
+#include <string>
+
+
+const int ALPHABET_SIZE = 26;
+
+// Структура узела дерева 
+struct TrieNode
+{
+    struct TrieNode* children[ALPHABET_SIZE];
+    // isEndOfWord - true, если ключ является концом слова
+    bool isEndOfWord;
+};
+
+TrieNode* getNewNode(void);
+void insert(TrieNode*, std::string);
+bool search(TrieNode*, std::string);
+bool isEmpty(TrieNode*);
+TrieNode* remove(TrieNode*, std::string, int depth = 0);
+void findWords(TrieNode*, std::string&, std::string&);
+void retOneWord(TrieNode*, char[], int, std::string&);
+
+#endif
