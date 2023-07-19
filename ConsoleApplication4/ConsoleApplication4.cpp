1e0d9ac9266f92eb55f4746c62440fc482373722
IlyaPoly <132166094+IlyaPoly@users.noreply.github.com>
2023-07-19 12:58:19 +0300

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include "Trie.h"
+
+int main()
+{
+    //По идее лучше было бы подгружать словарик из файла, сделать не проблема, но в задании не было
+    //решил лишний раз не усложнять жизнь)
+    std::string words[] = {
+        {"abele"},
+        {"abelmosk"},
+        {"aberdevine"},
+        {"aberrance"},
+        {"aberant"},
+    };
+    TrieNode* root = getNewNode();
+    for (auto& word : words)
+        insert(root, word);
+    
+    std::string res, find;
+    std::cout << "Write end press 'enter' : ";
+    std::cin >> find;
+    if (find.size() < 3)
+    {
+        std::cout << "short string!";
+        return 0;
+    }
+    findWords(root, find, res);
+    std::cout << "Possible words :\n";
+    std::cout << res;
+}
+
---
+++ ConsoleApplication4/Trie.cpp
@@ -0,0 +1,153 @@
+#include "Trie.h"
+#include "Trie.h"
+
+
+
+// Возвращет новый узел с пустыми детьми
+TrieNode* getNewNode(void)
+{
+    // Выделяем память по новый узел
+    struct TrieNode* pNode = new TrieNode;
+
+    // устанавливаем флаг конца слова в false
+    pNode->isEndOfWord = false;
+
+    // инициализируем детей нулевым уазателем
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        pNode->children[i] = nullptr;
+
+    return pNode;
+}
+
+
+// Вставляет ключ в дерево, если его нет, 
+// иначе если ключ явлется префксом узла дерева 
+// помечает вкачестве литового т.е. конец слова
+void insert(TrieNode* root, std::string key)
+{
+    TrieNode* node = root;
+
+    for (int i = 0; i < key.length(); i++)
+    {
+        // вычисляем индекс в алфите через смещение отнситьельно первой буквы
+        int index = key[i] - 'a';
+
+        // если указатель пустрой, т.е. детей с таким префиксом нет
+        // создаем новый узел
+        if (!node->children[index])
+            node->children[index] = getNewNode();
+        node = node->children[index];
+    }
+
+    // помечаем последний узлел как лист, т.е. конец слова
+    node->isEndOfWord = true;
+}
+
+// Возврашает true если ключ есть в дереве, иначе false 
+bool search(struct TrieNode* root, std::string key)
+{
+    struct TrieNode* node = root;
+
+    for (int i = 0; i < key.length(); i++)
+    {
+        int index = key[i] - 'a';
+        if (!node->children[index])
+            return false;
+
+        node = node->children[index];
+    }
+
+    return (node != nullptr && node->isEndOfWord);
+}
+
+// Вохвращает true если root имеет лист, иначе false 
+bool isEmpty(TrieNode* root)
+{
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        if (root->children[i])
+            return false;
+    return true;
+}
+
+// Рекурсивная функция удаления ключа из дерева 
+TrieNode* remove(TrieNode* root, std::string key, int depth)
+{
+    // Если дерево пустое 
+    if (!root)
+        return nullptr;
+
+    // если дошли до конца ключа 
+    if (depth == key.size()) {
+
+        // Этот узел больше не конец слова 
+        if (root->isEndOfWord)
+            root->isEndOfWord = false;
+
+        // Если ключ не евляется префиксом, удаляем его
+        if (isEmpty(root)) {
+            delete (root);
+            root = nullptr;
+        }
+
+        return root;
+    }
+
+    // Если не дошли до конца ключа, рекурсивно вызываем для ребенка 
+    // соответствующего символа 
+    int index = key[depth] - 'a';
+    root->children[index] = remove(root->children[index], key, depth + 1);
+
+    // Если у корня нет дочернего слова 
+    // (удален только один его дочерний элемент), 
+    // и он не заканчивается другим словом. 
+    if (isEmpty(root) && root->isEndOfWord == false) {
+        delete (root);
+        root = nullptr;
+    }
+
+    // возвращаем новый корень
+    return root;
+}
+
+
+void findWords(TrieNode* root, std::string& find, std::string& res)
+{
+    char buf[100]{};
+    int ind = 0;
+    if (root == nullptr)
+        return;
+    for (auto i = 0; i < find.size(); i++)
+    {
+        if (root->children[find[i] - 97] != nullptr)
+        {
+            root = root->children[find[i] - 97];
+            buf[i] = find[i];
+            ind++;
+        }
+        else
+        {
+            res = "Wrong!";
+            return;
+        }
+    }
+    retOneWord(root, buf, ind, res);
+}
+
+void retOneWord(TrieNode* root, char buf[], int ind, std::string& ret)
+{
+    if (root->isEndOfWord)
+    {
+        ret += std::string(buf) + '\n';
+        return;
+    }
+
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        if (root->children[i] != nullptr)
+        {
+            buf[ind] = i + 97;
+            retOneWord(root->children[i], buf, ind + 1, ret);
+            buf[ind] = 0;
+        }
+    }
+}
